f329e3c59fe20736f7f80ddf4ce4ffc367c811eb
Jorgelx <50906528+Jorgelx@users.noreply.github.com>
2021-12-27 03:40:36 +0100

    Revert "crear estructura del proyecto"
    This reverts commit 9c155174210b87748bfb56e38827746cbaf15e23.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PrincipalComponent } from './components/principal/principal.component';
 
-const routes: Routes = [
-  {path: "", component: PrincipalComponent},
-  {path: '**', redirectTo: '', pathMatch:'full'}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
